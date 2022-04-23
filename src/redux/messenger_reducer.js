390c2d628df16d56a21d18a94d1c451fc2ec1778
frost2329 <morozzzov.da@gmail.com>
2022-04-24 01:57:46 +0300

    47 - React JS практика - Делаем копию состояния в ProfileReducer

--- src/redux/messenger_reducer.js
+++ src/redux/messenger_reducer.js
@@ -24,18 +24,19 @@ let initialState = {
 const messengerReducer = (state = initialState, action) => {
     debugger;
     if (action.type === ADD_MESSAGE) {
-        let newMessage = {};
-        Object.assign(newMessage, state.newMessageData);
-        state.messageData.push(newMessage);
-        state.newMessageData.message_text = '';
+        return {
+            ...state,
+            messageData: [...state.messageData, state.newMessageData],
+            newMessageData: {...state.newMessageData, message_text: ''}
+        }
     }
     else if (action.type === UPDATE_NEW_MESSAGE_DATA) {
-        let newMessageData = {};
-        Object.assign(newMessageData, state.newMessageData);
-        newMessageData.message_text = action.message_text;
-        state.newMessageData = newMessageData;
+        return {
+            ...state,
+            newMessageData: {...state.newMessageData, message_text: action.message_text}
+        }
     }
-    return state;
+    else return {...state};
 }
 
 export const addMessageAC = () => ({
--- src/redux/profile_reducer.js
+++ src/redux/profile_reducer.js
@@ -42,18 +42,25 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     if (action.type === ADD_POST) {
-        let newPost = {};
-        Object.assign(newPost, state.myPostsState.newPostData);
-        state.myPostsState.postData.push(newPost);
-        state.myPostsState.newPostData.post_text = '';
+        return {
+            ...state,
+            myPostsState: {
+                postData: [...state.myPostsState.postData, state.myPostsState.newPostData],
+                newPostData: {...state.myPostsState.newPostData, post_text: ''}
+            }
+        }
     }
     else if (action.type === UPDATE_NEW_POST_DATA) {
-        let NewPostData = {};
-        Object.assign(NewPostData, state.myPostsState.newPostData);
-        NewPostData.post_text = action.post_text;
-        state.myPostsState.newPostData = NewPostData;
+        return {
+            ...state,
+            myPostsState: {
+                ...state.myPostsState,
+                newPostData: {...state.myPostsState.newPostData, post_text: action.post_text}
+            }
+        }
+
     }
-    return state;
+    else return state;
 }
 
 export const addPostAC = () => ({
