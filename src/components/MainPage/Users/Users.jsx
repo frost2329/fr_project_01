f362d6a77c1a11e6292c1a76503579ebeb1cfe42
frost2329 <morozzzov.da@gmail.com>
2022-05-10 03:13:18 +0300

    53 - React JS практика - классовый компонент (extends React.Component)

--- src/components/MainPage/Users/Users.jsx
+++ src/components/MainPage/Users/Users.jsx
@@ -3,17 +3,19 @@ import s from "./Users.module.css";
 import User from "./User/User";
 import * as axios from "axios";
 
-const Users = (props) => {
-    if (props.usersState.usersData.length === 0) {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then((response) => {
-                props.setUsers(response.data.items);
+                this.props.setUsers(response.data.items);
             });
     }
-    let usersElemets = props.usersState.usersData.map(u => <User user={u}
-                                                                 follow={props.follow}
-                                                                 unFollow={props.unFollow}/>);
-    return (
+    render() {
+        let usersElemets = this.props.usersState.usersData.map(user => <User user={user}
+                                                                             follow={this.props.follow}
+                                                                             unFollow={this.props.unFollow}/>);
+        return (
             <div className={s.users}>
                 <div>
                     {usersElemets}
@@ -22,7 +24,8 @@ const Users = (props) => {
                     <button>Show More</button>
                 </div>
             </div>
-    )
+        )
+    }
 }
 
 export default Users;
