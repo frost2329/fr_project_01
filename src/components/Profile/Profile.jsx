37bd32f4ac056febf0fc1f4a5f698daaef7e1757
frost2329 <73474164+frost2329@users.noreply.github.com>
2022-05-17 01:22:34 +0300

    60 - React JS - withRouter, props.match.params

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import Loading from "../common/Loading/Loading";
 import s from "./Profile.module.css";
+import user_avatar_img from "../../images/avatar_user_img.png";
 
 const Profile = (props) => {
     if (!props.profileState.profile) return <Loading isLoading={true}/>
@@ -10,7 +11,9 @@ const Profile = (props) => {
             <div className={s.narrow_column_wrap}>
                 <div className={s.page_block}>
                     <div className={s.avatar_img_bloc}>
-                        <img className={s.avatar_img} src={props.profileState.profile.photos.large} alt=""/>
+                        <img className={s.avatar_img} src={props.profileState.profile.photos.large
+                                                            ? props.profileState.profile.photos.large
+                                                            : user_avatar_img} alt=""/>
                     </div>
                     <div>
                         Редактировать
