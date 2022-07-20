3b23c5d0ba4f3bf87e0df89d7f6ade811822f6d5
frost2329 <73474164+frost2329@users.noreply.github.com>
2022-07-20 19:34:57 +0300

    93 - paginator, постраничный вывод - Практика ReactJS

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./Paginator.module.css";
 
 const Paginator = (props) => {
+    let portionSize = 10
     let pageCount = Math.ceil(props.totalCount / props.sizePage);
+    let portionCount = Math.ceil(pageCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber-1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+    let pages = [];
 
-    let pageNumberButtonsArray = [];
     for (let i = 1; i <= pageCount; i++) {
-        pageNumberButtonsArray.push(i);
+        pages.push(i);
     }
-    pageNumberButtonsArray = pageNumberButtonsArray.map((p) => {
+
+    pages = pages
+        .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+        .map((p) => {
         return (
             <span className={props.currentPage === p ? s.currentPageNumber : s.pageNumber}
                   onClick={() => {props.onPageNumber(p)}}>
@@ -16,7 +24,11 @@ const Paginator = (props) => {
             </span>
         )
     })
-    return <div>{pageNumberButtonsArray}</div>
+    return <div>
+        {portionNumber > 1 && <button onClick={()=>{setPortionNumber(portionNumber -1 )}}>PREV</button>}
+        {pages}
+        {portionNumber < portionCount && <button onClick={()=>{setPortionNumber(portionNumber +1 )}}>NEXT</button>}
+    </div>
 }
 
 export default Paginator;
